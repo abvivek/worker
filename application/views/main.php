26d150def1e0cc8ac969bb4684b666cb6abceb3f
vivek <abvivek1@gmail.com>
2017-11-23 22:32:59 +0530

    updated the main
    updated main home page

---
+++ application/views/main.php
@@ -0,0 +1,94 @@
+<hr /><div class="container text-center">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="text-muted">Project Management</h2>
+            <p>
+                <a href="#" class="btn btn-squared-default btn-primary">
+                    <i class="fa fa-key fa-5x"></i>
+                    <br /><br />
+                    Add Project
+                </a>
+                <a href="#" class="btn btn-squared-default btn-success">
+                    <i class="fa fa-clock-o fa-5x"></i>
+                    <br /><br />
+                    Time-sheet
+                </a>
+                <a href="#" class="btn btn-squared-default btn-warning">
+                    <i class="fa fa-book fa-5x"></i>
+                    <br /><br />
+                    My Projects
+                </a>
+                <a href="#" class="btn btn-squared-default btn-danger">
+                    <i class="fa fa-envelope-o fa-5x"></i>
+                    <br /><br />
+                    Mail
+                </a>
+                <a href="#" class="btn btn-squared-default btn-info">
+                    <i class="fa fa-bell fa-5x"></i>
+                    <br /><br />
+                    Pending Status
+                </a>
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="text-muted">Employee Portal</h2>
+            <p>
+                <a href="#" class="btn btn-squared-default-plain btn-primary">
+                    <i class="fa fa-user-md fa-5x"></i>
+                    <br /><br />
+                    Leave
+                </a>
+                <a href="#" class="btn btn-squared-default-plain btn-success">
+                    <i class="fa fa-file-code-o fa-5x"></i>
+                    <br /><br />
+                    My Log
+                </a>
+                <a href="#" class="btn btn-squared-default-plain btn-info">
+                    <i class="fa fa-compass fa-5x"></i>
+                    <br /><br />
+                    Extra
+                </a>
+                <a href="#" class="btn btn-squared-default-plain btn-warning">
+                    <i class="fa fa-pencil fa-5x"></i>
+                    <br /><br />
+                    Extra
+                </a>
+                <a href="#" class="btn btn-squared-default-plain btn-danger">
+                    <i class="fa fa-group fa-5x"></i>
+                    <br /> <br />
+                    Extra
+                </a>
+            </p>
+        </div>
+    </div>
+</div><hr />
+
+<style>
+    .btn-squared-default
+    {
+        width: 100px !important;
+        height: 100px !important;
+        font-size: 10px;
+    }
+
+    .btn-squared-default:hover
+    {
+        border: 3px solid white;
+        font-weight: 800;
+    }
+
+    .btn-squared-default-plain
+    {
+        width: 100px !important;
+        height: 100px !important;
+        font-size: 10px;
+    }
+
+    .btn-squared-default-plain:hover
+    {
+        border: 0px solid white;
+    }
+</style>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,9 @@
     <head>
         <title>Worker</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/jquery-ui.css">        
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/font-awesome.min.css" />
+    
         
         <script src="<?php echo base_url() ?>assets/js/jquery-3.2.1.min.js"></script>
         <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>        
